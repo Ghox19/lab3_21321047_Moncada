688b9534ec41d287a9d765b939aca33872af957a
Ghox19 <gonzalo.moncada19@gmail.com>
2023-12-01 17:20:09 -0300

    feat(Chatbot):Chatbot java class created & get/set

---
+++ codigofuente_21321047_MoncadaSanchez/src/main/java/lab3/Paradigmas/Chatbot.java
@@ -0,0 +1,63 @@
+package lab3.Paradigmas;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class Chatbot implements inChatbot{
+    private int chatbotId;
+    private String name;
+    private String welcomeMessage;
+    private int startFlowId;
+    private ArrayList<Flow> flows;
+
+    public Chatbot(int chatbotId, String name, String welcomeMessage, int startFlowId, ArrayList<Flow> flows) {
+        this.chatbotId = chatbotId;
+        this.name = name;
+        this.welcomeMessage = welcomeMessage;
+        this.startFlowId = startFlowId;
+        this.flows = verifyFlows(flows);
+    }
+
+    public int getChatbotId() {
+        return chatbotId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getWelcomeMessage() {
+        return welcomeMessage;
+    }
+
+    public int getStartFlowId() {
+        return startFlowId;
+    }
+
+    public ArrayList<Flow> getFlows() {
+        return flows;
+    }
+
+    public void setFlows(ArrayList<Flow> flows) {
+        this.flows = flows;
+    }
+
+    public ArrayList<Flow> verifyFlows(ArrayList<Flow> flows) {
+        ArrayList<Flow> flowsClean = new ArrayList<>();
+        for (Flow flow : flows) {
+            int IdActual = flow.getFlowId();
+            boolean IdExiste = false;
+            for (Flow flw : flowsClean) {
+                if (flw.getFlowId() == IdActual) {
+                    IdExiste = true;
+                    break;
+                }
+            }
+            if (!IdExiste) {
+                flowsClean.add(flow);
+            }
+        }
+        return flowsClean;
+    }
+
+    }
---
+++ codigofuente_21321047_MoncadaSanchez/src/main/java/lab3/Paradigmas/inChatbot.java
@@ -0,0 +1,4 @@
+package lab3.Paradigmas;
+
+public interface inChatbot {
+}
